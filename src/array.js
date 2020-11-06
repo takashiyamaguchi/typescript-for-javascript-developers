ebe8425cf255b1e6581e59009f85b67b8829be9a
yama <tkshyamaguchi45@gmail.com>
2020-11-06 10:53:08 +0900

    array.ts

---
+++ src/array.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var numbers = [1, 2, 3];
+var numbers2 = [1, 2, 3];
+var strings2 = ['tokyo', 'osaka', 'kyoto'];
+var strings = ['typescript', 'javascript', 'coffeescript'];
+var nijigenHairetsu = [
+    [150, 150],
+    [150, 150]
+];
+var hairetsu = [1, false, 'Japan'];
---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1,2,3];
+
+let numbers2: Array<number> = [1,2,3];
+let strings2: Array<string> = ['tokyo', 'osaka', 'kyoto'];
+
+let strings: string[] = ['typescript', 'javascript', 'coffeescript'];
+
+let nijigenHairetsu: number[][] = [
+  [150, 150],
+  [150, 150]
+]
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
