a3799a0ada597a84c8038243b689e54c6e51032a
yama <tkshyamaguchi45@gmail.com>
2020-11-28 07:22:51 +0900

    rest-parameters

--- src/rest-parameters.ts
+++ src/rest-parameters.ts
@@ -1,12 +1,12 @@
-export { }
+export { };
 
 const reducer = (accumulator: number, currentValue: number) => {
   console.log({ accumulator, currentValue });
   return accumulator + currentValue;
-};
+}
 
 const sum: (...values: number[]) => number = (...values: number[]): number => {
   return values.reduce(reducer);
-};
+}
 
 sum(1, 2, 3, 4, 5);
