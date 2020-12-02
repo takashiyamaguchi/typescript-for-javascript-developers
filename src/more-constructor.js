bf5e6726dd9e69b592c5cfd445b973a6c16c6cba
yama <tkshyamaguchi45@gmail.com>
2020-12-02 11:53:06 +0900

    more constructor

---
+++ src/more-constructor.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Person;
+}());
+var me = new Person('はむさん', 43);
+console.log(me);
